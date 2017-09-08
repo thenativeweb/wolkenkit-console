412241871d76095868e7da272233b9e942238dc7
Jan-Hendrik Grundhöfer <jan-hendrik.grundhoefer@web.de>
2017-09-08 15:26:14 +0200

    If is token set, login directly (quick and dirty implementation).

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import App from './App';
+import application from './readModel/application';
+import { connectToBackend } from './writeModel/backend';
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
+
+const token = localStorage.getItem(`id_token_${application.authentication.clientId}`);
+
+if (token) {
+  connectToBackend();
+}
 
 render(<App />, document.querySelector('#app'));
