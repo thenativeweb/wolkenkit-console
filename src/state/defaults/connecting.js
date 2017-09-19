ee61ca6c64c83d688f9ca61632d21c330441c023
Jan-Hendrik Grundhöfer <jan-hendrik.grundhoefer@web.de>
2017-09-19 15:13:38 +0200

    Remove personal default values.

--- src/state/defaults/connecting.js
+++ src/state/defaults/connecting.js
@@ -2,10 +2,10 @@ const connecting = {
   host: 'local.wolkenkit.io',
   port: 3000,
   authentication: {
-    identityProviderUrl: 'https://grundhoeferj.eu.auth0.com/authorize',
-    clientId: 'Pny9tYykOORUAPC3JcIB8XMAJ_8nsv6c',
-    scope: 'profile',
-    strictMode: false
+    identityProviderUrl: '',
+    clientId: '',
+    scope: '',
+    strictMode: true
   },
   error: undefined
 };
