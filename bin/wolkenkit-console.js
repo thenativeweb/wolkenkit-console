355fc0107f07d8ad17b60fe0d405f04341c2ed15
Jan-Hendrik Grundhöfer <jan-hendrik.grundhoefer@web.de>
2017-12-01 12:53:48 +0100

    Fix startup on windows.

--- bin/wolkenkit-console.js
+++ bin/wolkenkit-console.js
@@ -8,6 +8,16 @@
 
 const path = require('path');
 
+// On Windows, nvm has the strange behavior to store applications inside of a
+// node_modules directory. Hence finding the application's root path fails,
+// which is tried in flaschenpost. To fix this, we manually set the root path
+// here.
+if (process.platform === 'win32' && __dirname.includes('\\nvm\\')) {
+  /* eslint-disable no-process-env */
+  process.env.APP_ROOT_PATH = path.join(__dirname, '..');
+  /* eslint-enable no-process-env */
+}
+
 const express = require('express'),
       flaschenpost = require('flaschenpost'),
       httpsOrHttp = require('https-or-http'),
